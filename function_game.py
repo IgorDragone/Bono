9894103557ae4b684a4b51f00fcf719d56386d6f
Igor Dragone <alu0101469652@ull.edu.es>
2023-11-22 14:05:49 +0000

    1

--- function_game.py
+++ function_game.py
@@ -1,43 +1,42 @@
 import turtle
-import sys
 
 # Mover la tortuga hacia delante
 def move_forward():
     print("¿Cuántos metros quieres avanzar?")
     x = input()
-    turtle.forward(x)
-    print("La tortuga ha avanzado " + x + " metros")
+    turtle.forward(float(x))
+    print("La tortuga ha avanzado " + x + " metros\n")
 
 # Mover la tortuga hacia atrás
 def move_backward():
     print("¿Cuántos metros quieres retroceder?")
     x = input()
-    turtle.backward(x)
-    print("La tortuga ha retrocedido " + x + " metros")
+    turtle.backward(float(x))
+    print("La tortuga ha retrocedido " + x + " metros\n")
 
 # Girar la tortuga a la derecha
 def move_right():
     print("¿Cuántos grados quieres girar a la derecha?")
     x = input()
-    turtle.right(x)
-    print("La tortuga ha girado " + x + " grados a la derecha")
+    turtle.right(float(x))
+    print("La tortuga ha girado " + x + " grados a la derecha\n")
 
 # Girar la tortuga a la izquierda
 def move_left():
     print("¿Cuántos grados quieres girar a la izquierda?") 
     x = input()
-    turtle.left(x)
-    print("La tortuga ha girado " + x + " grados a la izquierda")
+    turtle.left(float(x))
+    print("La tortuga ha girado " + x + " grados a la izquierda\n")
 
 # Levantar la tortuga
 def move_penup():
     turtle.penup()
-    print("La tortuga ha sido levantada, ya no dibujará")
+    print("La tortuga ha sido levantada, ya no dibujará\n")
 
 # Bajar la tortuga
 def move_pendown():
     turtle.pendown()
-    print("La tortuga ha sido bajada, ahora dibujará")
+    print("La tortuga ha sido bajada, ahora dibujará\n")
 
 # Dibujar un circulo
 def move_circle():
@@ -45,13 +44,13 @@ def move_circle():
     x = input()
     print("¿Cuántos grados quieres que gire la tortuga? (360 para un circulo completo))")
     y = input()
-    turtle.circle(x,y)
-    print("La tortuga ha dibujado un circulo de radio " + x + " y ha girado " + y + " grados")
+    turtle.circle(float(x),float(y))
+    print("La tortuga ha dibujado un circulo de radio " + x + " y ha girado " + y + " grados\n")
 
 # Resetear la tortuga
 def reset():
     turtle.reset()
-    print("La tortuga ha sido reseteada")
+    print("La tortuga ha sido reseteada\n")
 
 
 
@@ -66,12 +65,12 @@ def play_game():
     print("Bienvenido a Geometric Fun(ction)")
     print("En este juego deberás controlar una tortuga para dibujar figuras geométricas")
 
-    acciones = "0. Salir\n1. Avanzar\n2. Retroceder\n3. Girar a la derecha\n4. Girar a la izquierda\n5. Levantar a la tortuga\n6. Bajar a la tortuga\n7. Dibujar un circulo\n8. Reset\n 9. Mostrar acciones disponibles\n"
+    acciones = "0. Salir\n1. Avanzar\n2. Retroceder\n3. Girar a la derecha\n4. Girar a la izquierda\n5. Levantar a la tortuga\n6. Bajar a la tortuga\n7. Dibujar un circulo\n8. Reset\n9. Mostrar acciones disponibles\n"
     print("Las acciones disponibles para el jugador son: ")
     print(acciones)
 
     # Limpiamos la consola y la tortuga
-    sys.clear()
+    
     turtle.clear()
 
     action = -1
@@ -99,7 +98,7 @@ def play_game():
         elif action == "9":
             print(acciones)
         else:
-            print("Acción no válida")
+            print("Acción no válida\n")
 
 
     # Salir del juego
